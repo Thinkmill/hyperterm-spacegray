dd3c3f0106058397a5c294ed50958c710fb74f16
Max <contact@mstoiber.com>
2016-07-18 10:35:34 +0200

    Add proper colors

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 // Constants
 const backgroundColor = '#343d46';
 const foregroundColor = '#c0c5ce';
-const selectionBackground = '#4f5b66';
-const selectionForeground = '#a7adba';
+
+// Colors
+// TODO Figure out good color for Cyan
+const colors = [
+  backgroundColor,
+  '#9B4444', // red
+  '#68A281', // green
+  '#FFAA2C', // yellow
+  '#0098FF', // blue
+  '#DB2FF4', // pink
+  '#0098FF', // cyan
+  '#a7adba', // light gray
+  '#4f5b66', // medium gray
+  '#9B4444', // red
+  '#68A281', // green
+  '#FFAA2C', // yellow
+  '#0098FF', // blue
+  '#DB2FF4', // pink
+  '#0098FF', // cyan
+  '#ffffff', // white
+  foregroundColor
+];
 
 // Apply theme
 exports.decorateConfig = (config) => (
   Object.assign({}, config, {
     backgroundColor,
     foregroundColor,
-    borderColor: dividerColor,
-    cursorColor: selectionForeground,
-    // TODO Add colors array here, figure out what each color is used for
-    colors: [
-      '#FFFFFF'
-    ],
+    colors,
     css: `
       ${config.css || ''}
       .tab_tab {
